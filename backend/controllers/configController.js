c45ee2826039c8b5d3b9579a58eb601962c24deb
Uday Kiran <udaykiran007.uu@gmail.com>
2026-02-12 23:43:54 +0530

    env test console

--- backend/controllers/configController.js
+++ backend/controllers/configController.js
@@ -1,4 +1,10 @@
 exports.getConfig = (req, res) => {
+  console.log("[DEBUG-CONFIG] Env Vars Check:", {
+    OTP: process.env.OTP_TEST_MODE,
+    PAYMENT: process.env.PAYMENT_TEST_MODE,
+    SHIPPING: process.env.SHIPPING_MODE
+  });
+  
   res.json({
     otpTestMode: process.env.OTP_TEST_MODE === "true",
     paymentTestMode: process.env.PAYMENT_TEST_MODE === "true",
